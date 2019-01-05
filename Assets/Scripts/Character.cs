9a8ee3861178009f75281f4ad1558d3a6e587302
R-Marc-Sal <rmarcsal@gmail.com>
2019-01-05 14:01:42 -0800

    Added WASD movement for May
    test

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player and enemies inherit from this class
+public abstract class Character : MonoBehaviour
+{
+
+    // Encapsulate variables
+    [SerializeField]
+    private float speed;
+    protected Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Protected virtual overrides the parent's Update function
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        // The transform is just the absolute position of the object
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void AnimateMovement(Vector2 direction) {
+
+    }
+}
