832e414683fe059ddf5e0b66c75772234fc7ebe4
R-Marc-Sal <rmarcsal@gmail.com>
2019-01-12 12:25:14 -0800

    Attempt at races + equipment system
    Work in progress

---
+++ Assets/Scripts/General/Base/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public UnitStat Strength;
+}
+
+
+public class Item
+{
+    StatModifier mod1, mod2;
+
+    public string name;
+    public void Equip(Character c)
+    {
+        // Modifiers need to be stored in variables fists before adding them to the stat
+        mod1 = new StatModifier(10, StatModType.Flat);
+        mod2 = new StatModifier(0.1f, StatModType.PercentMult);
+        c.Strength.AddModifier(mod1);
+        c.Strength.AddModifier(mod2);
+    }
+
+    public void Unequip(Character c)
+    {
+        c.Strength.RemoveModifier(mod1);
+        c.Strength.RemoveModifier(mod2);
+    }
+}
---
+++ Assets/Scripts/General/Classes/Knight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : Unit
+{
+
+    public string name;
+    public List<Equipment> equipmentList;
+
+
+    public override void MarkAsAttacking(Unit other)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MarkAsDefending(Unit other)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MarkAsDestroyed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MarkAsFinished()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MarkAsFriendly()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MarkAsSelected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UnMark()
+    {
+        throw new System.NotImplementedException();
+    }
+}
