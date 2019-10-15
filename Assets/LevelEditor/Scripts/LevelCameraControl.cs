2d6cd52db778ad829259b5c08e8a3024b52557f0
Varler <breslinjarod@gmail.com>
2019-10-15 16:23:13 -0700

    Robustified Camera Movement Code
    -Prevented camera behavior when out of frame (useful for developers), preventing zooming/panning/rotating while not interacting with the game
    -Prevented "skipping" bug in which movement/rotation would jump wildly when re-entering frame due to using previous, distant mouse position where mouse exited frame

--- Assets/LevelEditor/Scripts/LevelCameraControl.cs
+++ Assets/LevelEditor/Scripts/LevelCameraControl.cs
@@ -16,42 +16,76 @@ public class LevelCameraControl : MonoBehaviour
     Camera cam;
     float zoomAmount = 1;
     Vector2 oldMousePos;
-
+	bool wasInside = false; //was mouse inside the window in the previous frame?
+	
     void Awake()
     {
         cam = GetComponent<Camera>();
         oldMousePos = Input.mousePosition;
     }
-
+	
+	private bool MouseInWindow()
+	{
+		//is the mouse within the bounds of the window as defined by the camera?
+		var view = cam.ScreenToViewportPoint(Input.mousePosition);
+		bool isInside = view.x > 0 && view.x < 1 && view.y > 0 && view.y < 1;
+		if (!isInside)
+		{
+			wasInside = false;
+		}
+		return isInside;
+	}
+	
+	private bool MouseEnteredWindow()
+	{
+		//is this the frame in which the mouse entered the window?
+		bool enteredWindow = !wasInside && MouseInWindow();
+		return enteredWindow;
+	}
+	
     // Update is called once per frame
     void Update()
     {
-		//Rotation Code
-        zoomAmount += Input.mouseScrollDelta.y;
-        zoomAmount = Mathf.Clamp(zoomAmount, 1f, 12f);
-        var movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        movement = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * movement;
-        movement = transform.InverseTransformDirection(movement);
-        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.Self);
-
-        if (Input.GetMouseButton(1))
-        {
-            var mouseXdelta = (Input.mousePosition.x - oldMousePos.x) / Screen.width;
-            transform.RotateAround(transform.position + transform.forward * 20f, Vector3.up, mouseXdelta * rotationSpeed * Time.deltaTime * 365f);
-            //transform.Rotate(new Vector3(0, , 0), Space.World);
-        }
-        var scale = Mathf.Max(1f,Mathf.Round(zoomAmount * (Screen.height / referenceHeight)));
-        var halfHeight = Screen.height / (2f * pixelsPerUnit * scale);
-        cam.orthographicSize = halfHeight;
-		
-		//Translation code
-		if (Input.GetMouseButton(2))
+		if (!MouseInWindow())
 		{
-			var mouseXdelta = (Input.mousePosition.x - oldMousePos.x) / Screen.width;
-			var mouseYdelta = (Input.mousePosition.y - oldMousePos.y) / Screen.height;
-			transform.Translate(-mouseXdelta*40f/(zoomAmount), -mouseYdelta*40f/(zoomAmount), 0f);
+			Debug.Log("mouse was not in window this frame");
 		}
-		
-        oldMousePos = Input.mousePosition;
+		else if (MouseEnteredWindow())
+		{
+			//Mouse must be in window per the previous if statement
+			//give the game a frame to store the actual mouse position to prevent "skipping"
+			oldMousePos = Input.mousePosition;
+			wasInside = true;
+		}
+		else
+		{
+			//Rotation Code
+			zoomAmount += Input.mouseScrollDelta.y;
+			zoomAmount = Mathf.Clamp(zoomAmount, 1f, 12f);
+			var movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+			movement = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * movement;
+			movement = transform.InverseTransformDirection(movement);
+			transform.Translate(movement * movementSpeed * Time.deltaTime, Space.Self);
+
+			if (Input.GetMouseButton(1))
+			{
+				var mouseXdelta = (Input.mousePosition.x - oldMousePos.x) / Screen.width;
+				transform.RotateAround(transform.position + transform.forward * 20f, Vector3.up, mouseXdelta * rotationSpeed * Time.deltaTime * 365f);
+				//transform.Rotate(new Vector3(0, , 0), Space.World);
+			}
+			var scale = Mathf.Max(1f,Mathf.Round(zoomAmount * (Screen.height / referenceHeight)));
+			var halfHeight = Screen.height / (2f * pixelsPerUnit * scale);
+			cam.orthographicSize = halfHeight;
+			
+			//Translation code
+			if (Input.GetMouseButton(2))
+			{
+				var mouseXdelta = (Input.mousePosition.x - oldMousePos.x) / Screen.width;
+				var mouseYdelta = (Input.mousePosition.y - oldMousePos.y) / Screen.height;
+				transform.Translate(-mouseXdelta*40f/(zoomAmount), -mouseYdelta*40f/(zoomAmount), 0f);
+			}
+			
+			oldMousePos = Input.mousePosition;
+		}
     }
 }
